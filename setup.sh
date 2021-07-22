cb54bed2aab3c56b401b0a67c00fbfae0b40b052
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-07-22 15:15:38 +0100

    add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+ln -sv "$(pwd)"/.local/bin/sftp_uploader ~/.local/bin/
+ln -sv "$(pwd)"/.local/bin/config.ini ~/.local/bin/
+ln -sv "$(pwd)"/.local/bin/screenshot ~/.local/bin
+ln -sv "$(pwd)"/.zshrc ~
